c049cea68dacdcea87b289bd88618565e560391e
Brijesh Radadiya <radadiyabrijesh002@gmail.com>
2025-02-24 21:46:21 +0530

    Add files via upload

---
+++ About.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function about() {
+  return (
+    <div>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et officiis neque, tenetur perspiciatis saepe a similique maxime id amet unde quis, quaerat sapiente quam doloribus repudiandae ipsum quibusdam earum, nesciunt inventore aspernatur! Modi, explicabo. Perspiciatis exercitationem voluptatem quia sequi provident ut reiciendis atque ullam, esse qui est nam unde, accusamus ab eligendi recusandae ad a odio, repudiandae nostrum distinctio ipsa commodi omnis nulla? Deserunt minima, eius autem repellat quidem inventore dignissimos nulla adipisci similique ex at vitae consequatur? Ipsa dolor voluptatum nulla veniam rerum, illo nostrum maiores aliquid a aut maxime saepe dicta veritatis consequuntur vel itaque laborum mollitia eos.
+      </p>
+    </div>
+  )
+}
---
+++ Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function footer() {
+  
+  return (
+    <footer className='bg-dark text-light py-3'  >
+      <p className='text-center'>
+        Copyright &copy; MyTodosList.com
+      </p>
+    </footer>
+  )
+}
+
